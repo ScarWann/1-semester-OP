5f29a7b1ebc91b82da1720b1c4a7676f5f2de6f0
ScarWann <lyosha_keliukh@outlook.com>
2025-11-24 01:23:12 +0200

    Readded PR3 properly

--- PR3/main.c
+++ PR3/main.c
@@ -7,39 +7,26 @@
 #else
     #include <stdio_ext.h>
 #endif
+#include "main.h"
 
 
-#define MAX_X 1e304
-#define MIN_E 1e-16
-#define MAX_K 1e6
-
-
-void interaction_cycle(void);
-inline bool valid_e(double e);
-inline bool valid_k(double k);
-inline bool valid_x(double x, double e, short k);
-double root(double x, int k, double e, unsigned short precision);
-
-static double extra_scanf(void);
-static bool end_input(void);
-static inline void flush_stdin(void);
-static double root_(double x, int k, double e, unsigned short precision);
 
 int main() {
     printf("Program for calculating square roots up to a certain precision.\n"
            "You can exit at any time by pressing Ctrl+D\n");
     do {
-        loop();
+        interaction_cycle();
     } while (!end_input());
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-void loop() {
-    bool success = true; 
+void interaction_cycle() {
+    bool success; 
+
     double e = 0;
     printf("Enter epsilon (must be a positive number in range [%.0le, 0.1], prefferably in exponential form (i.e. 1e-6)): ", MIN_E);
     do {
-        e = extraScanf();
+        e = extra_scanf();
         success = valid_e(e);
         if (!success) printf("Please enter a number in the specified range: ");
     } while (!success);
@@ -49,16 +36,19 @@ void loop() {
     int k = 0;
     printf("Enter k (must be an integer less than or equal to %.0e): ", MAX_K);
     do {
-        temp = extraScanf();
-    } while (!validK(temp));
+        temp = extra_scanf();
+        success = valid_k(temp);
+        if (!success) printf("Please enter an integer in the specified range: ");
+    } while (!success);
     k = (int)temp;
 
     double x = 0;
     printf("Enter x (must have a square root of power %d and be in range [%.0e, %.0e]): ", k, e, MAX_X);
     do {
-        x = extraScanf();
-        printf("Please enter a number that has the specified root in the valid range: ");
-    } while (!validX(x, e, k));
+        x = extra_scanf();
+        success = valid_x(x, e, k);
+        if (!success) printf("Please enter a number that has the specified root in the valid range: ");
+    } while (!success);
 
     if (x && x != 1) {
         printf("The final root value is %.*lf\n", precision, root(x, k, e, precision));
@@ -67,35 +57,23 @@ void loop() {
     }
 }
 
-inline bool valid_e(double e) {
+extern inline bool valid_e(double e) {
     return (0.1 >= e && e >= MIN_E);
 }
 
-
-inline bool valid_x(double x, double e, short k) {
+extern inline bool valid_x(double x, double e, short k) {
     if (isnan(x) || fabs(x) > MAX_X || fabs(x) < e) return false;
     if (k % 2 == 0 && x <= 0) return false;
     if (k < 0 && x == 0) return false;
     return true;
 }
 
-bool valid_x(double x, double e, short k) {
-    if (!(fabs(x) <= MAX_X && fabs(x) >= e)) {
-        return false;
-    } else if (k > 0 && (k % 2 == 0) && (x < 0)) {
-        return false;
-    } else if (k > 0) {
-        return true;
-    } else if ((k % 2 == 0) && (x <= 0)) {
-        return false;
-    } else if (x == 0) {
-        return false;
-    }
-    return true;
+extern inline bool valid_k(double k) {
+    return ((int)k == k && fabs(k) <= MAX_K && (int)k != 0);
 }
 
 
-double extra_scanf() {
+static double extra_scanf() {
     short success = 0;
     double result = 0;
     success = scanf("%lf", &result);
@@ -105,7 +83,7 @@ double extra_scanf() {
     } else if (!success){
         result = NAN;
     }
-    flushStdin();
+    flush_stdin();
     return result;
 }
 
@@ -117,7 +95,7 @@ static bool end_input() {
         printf("End of input detected. Closing program.\n");
         return 1;
     }
-    flushStdin();
+    flush_stdin();
     return 0;
 }
 
@@ -133,19 +111,26 @@ static void inline flush_stdin(void) {
 #endif 
 }
 
-static double root_(double x, int k, double e, unsigned short precision) {
+double root(double x, int k, double e, unsigned short precision) {
+    double out;
+    unsigned int iters;
+    if (k > 0) {
+        iters = root_(x, k, e, precision, &out);
+    } else {
+        iters = root_(1./x, -k, e, precision, &out);
+    }
+    printf("Calculation finished in %d iterations\n", iters);
+    return out;
+}
+
+static inline double root_(double x, int k, double e, unsigned short precision, double *out) {
     double y = 1, d = 1;
     unsigned int i = 0;
     while (fabs(d) > e) {
         d = (x / pow(y, k - 1) - y) / k;
         y += d;
         i++;
-        if (i < 1e3) {
-            printf("Iteration %d: y = %.*le, delta = %.*le\n", i, precision, y, precision, d);
-        } else if (i == 1e3) {
-            printf("Exceeded 1000 iterations, detailed output turned off.\n");
-        }
     }
-    printf("Calculated root in %u iterations\n", i);
-    return y;
+    *out = y;
+    return i;
 }
---
+++ PR3/main.h
@@ -0,0 +1,20 @@
+#ifndef PR3
+#define PR3
+
+#define MAX_X 1e100
+#define MIN_E 1e-16
+#define MAX_K 1e6
+
+
+extern void interaction_cycle(void);
+extern inline bool valid_e(double e);
+extern inline bool valid_k(double k);
+extern inline bool valid_x(double x, double e, short k);
+extern double root(double x, int k, double e, unsigned short precision);
+
+static double extra_scanf(void);
+static bool end_input(void);
+static inline void flush_stdin(void);
+static inline double root_(double x, int k, double e, unsigned short precision, double *out);
+
+#endif
